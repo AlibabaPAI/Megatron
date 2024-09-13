d53e7f5a33e860c3a3fa6732a9bbee8887fa5fcb
luzhan <luzhan.lz@alibaba-inc.com>
2024-09-13 10:36:39 +0800

    feat: add llama3-70b script

--- examples/llama3-70b.sh
+++ examples/llama3-70b.sh
@@ -1,2 +1,9 @@
-# fsdp=32
-bash examples/run-megatron.sh --random-init --mbs 1 --gbs 32 --attn-type flash --model-size 70B --seq-len 2048 --gc --gc-cnt 80
+# ========= seq_len=8192 mbs=1 pp=4 tp=8 gc_cnt=80 GA =========
+bash examples/run-megatron.sh --random-init --mbs 1 --gbs 256 --attn-type flash --model-size 70B --seq-len 8192 --tp 8 --pp 4 --vp 5 --gc --gc-cnt 80
+
+# ========= seq_len=8192 mbs=1 pp=4 tp=8 gc_cnt=1 GA =========
+bash examples/run-megatron.sh --random-init --mbs 1 --gbs 256 --attn-type flash --model-size 70B --seq-len 8192 --tp 8 --pp 4 --vp 5 --sp --gc --gc-cnt 1 # OPTIMAL
+
+# ========= seq_len=8192 mbs=1 pp=4 tp=8 gc_cnt=1 GA =========
+bash examples/run-megatron.sh --random-init --mbs 1 --gbs 64 --attn-type flash --model-size 70B --seq-len 8192 --tp 8 --pp 4 --vp 5 --sp --gc --gc-cnt 1
+
